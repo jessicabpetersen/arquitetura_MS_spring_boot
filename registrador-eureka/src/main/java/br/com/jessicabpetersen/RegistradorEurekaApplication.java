df24b469e160528227e56ef0aeabb0f8e24bc4c3
Jessica <jessica041ster@gmail.com>
2022-10-10 14:40:36 -0300

    configurando eureka server

--- registrador-eureka/src/main/java/br/com/jessicabpetersen/RegistradorEurekaApplication.java
+++ registrador-eureka/src/main/java/br/com/jessicabpetersen/RegistradorEurekaApplication.java
@@ -2,8 +2,10 @@ package br.com.jessicabpetersen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class RegistradorEurekaApplication {
 
 	public static void main(String[] args) {
