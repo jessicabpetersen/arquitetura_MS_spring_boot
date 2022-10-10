0ebccd91dc7370ade729484740bc0787b0698d2c
Jessica <jessica041ster@gmail.com>
2022-10-10 13:39:38 -0300

    adicionando a dependencia do ribbon - loader balancer - inicial

--- payroll/src/main/java/br/com/jessicabpetersen/PayrollApplication.java
+++ payroll/src/main/java/br/com/jessicabpetersen/PayrollApplication.java
@@ -2,8 +2,10 @@ package br.com.jessicabpetersen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@RibbonClient(name = "worker")
 @EnableFeignClients
 @SpringBootApplication
 public class PayrollApplication {
